51a3872a50ad4a8f2d744f7befaffe55c3f1dea0
Sabrina0908 <sabrinanurhasanah09>
2026-07-27 10:31:29 +0700

    Fix PORT variable for deployment

--- wa-api/index.js
+++ wa-api/index.js
@@ -66,7 +66,7 @@ app.post('/send-message', async (req, res) => {
 });
 
 // Start the Express server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`\n=========================================`);
     console.log(`Local WhatsApp API Server is running on http://localhost:${PORT}`);
